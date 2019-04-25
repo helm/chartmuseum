259dc866d98921b0366eed2e73de3f0e2b14b38f
Debarshi <debarshi@reynencourt.com>
2019-04-25 23:55:10 +0200

    etcd support added
    Signed-off-by: debarshibasak  <debar5h16a5ak@gmail.com>
    Signed-off-by: debarshibasak <debar5h16a5ak@gmail.com>
    
     Author:    debarshibasak <debar5h16a5ak@gmail.com>
     Date:      Thu Apr 25 23:55:10 2019 +0200

--- cmd/chartmuseum/main.go
+++ cmd/chartmuseum/main.go
@@ -125,6 +125,8 @@ func backendFromConfig(conf *config.Config) storage.Backend {
 		backend = openstackBackendFromConfig(conf)
 	case "baidu":
 		backend = baiduBackendFromConfig(conf)
+	case "etcd":
+		backend = etcdBackendFromConfig(conf)
 	default:
 		crash("Unsupported storage backend: ", storageFlag)
 	}
@@ -197,6 +199,18 @@ func openstackBackendFromConfig(conf *config.Config) storage.Backend {
 		conf.GetString("storage.openstack.prefix"),
 		conf.GetString("storage.openstack.region"),
 		conf.GetString("storage.openstack.cacert"),
+	))
+}
+
+func etcdBackendFromConfig(conf *config.Config) storage.Backend {
+	crashIfConfigMissingVars(conf, []string{"storage.etcd.endpoints",
+		"storage.etcd.cafile", "storage.etcd.certfile","storage.etcd.keyfile","storage.etcd.prefix" })
+	return storage.Backend(storage.NewEtcdCSBackend(
+		conf.GetString("storage.etcd.endpoints"),
+		conf.GetString("storage.etcd.cafile"),
+		conf.GetString("storage.etcd.certfile"),
+		conf.GetString("storage.etcd.keyfile"),
+		conf.GetString("storage.etcd.prefix"),
 	))
 }
 
