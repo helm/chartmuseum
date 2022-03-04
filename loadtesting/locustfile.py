a505543df17417e9bc8b7fc7c512cb46df21b5be
Casey Buto <cbuto@d2iq.com>
2022-03-04 17:34:59 -0500

    test: Update to the latest locust version
    Signed-off-by: Casey Buto <cbuto@d2iq.com>

--- loadtesting/locustfile.py
+++ loadtesting/locustfile.py
@@ -1,39 +1,36 @@
-from locust import HttpLocust, TaskSet
+from locust import HttpUser, TaskSet, task
 import tarfile
 import io
 
 patch_version = 1
-chart_post_field_name = 'chart'
-
-def index(l):
-    l.client.get("/index.yaml")
-
-def post_new_chart(l):
-    global patch_version
-
-    # Create dummy 'chartmuseum-loadtest' chart package for which we only increment the patch version
-    chart_name = 'chartmuseum-loadtest'
-    chart_version = '0.0.%d' % patch_version
-    patch_version += 1
-    chart_fn = '%s-%s.tgz' % (chart_name, chart_version)
-
-    tgz_buf = io.BytesIO()
-    t = tarfile.open(mode = "w:gz", fileobj=tgz_buf)
-    chart_content = b'name: %s\nversion: %s\n' % (chart_name.encode('utf8'), chart_version.encode('utf8'))
-    tarinfo = tarfile.TarInfo('%s/Chart.yaml' % chart_name)
-    tarinfo.size = len(chart_content)
-    t.addfile(tarinfo=tarinfo, fileobj=io.BytesIO(chart_content))
-    t.close()
-    tgz_buf.seek(0)
-
-    l.client.post('/api/charts', files={chart_post_field_name: (chart_fn, tgz_buf)})
-
 
 class UserBehavior(TaskSet):
-    tasks = {index: 10, post_new_chart: 1}
+    @task(10)
+    def index(self):
+        self.client.get("/index.yaml")
 
+    @task(1)
+    def post_new_chart(self):
+        global patch_version
+        # Create dummy 'chartmuseum-loadtest' chart package for which we only increment the patch version
+        chart_post_field_name = 'chart'
+        chart_name = 'chartmuseum-loadtest'
+        chart_version = '0.0.%d' % patch_version
+        patch_version += 1
+        chart_fn = '%s-%s.tgz' % (chart_name, chart_version)
 
-class WebsiteUser(HttpLocust):
-    task_set = UserBehavior
+        tgz_buf = io.BytesIO()
+        t = tarfile.open(mode = "w:gz", fileobj=tgz_buf)
+        chart_content = b'name: %s\nversion: %s\n' % (chart_name.encode('utf8'), chart_version.encode('utf8'))
+        tarinfo = tarfile.TarInfo('%s/Chart.yaml' % chart_name)
+        tarinfo.size = len(chart_content)
+        t.addfile(tarinfo=tarinfo, fileobj=io.BytesIO(chart_content))
+        t.close()
+        tgz_buf.seek(0)
+
+        self.client.post('/api/charts', files={chart_post_field_name: (chart_fn, tgz_buf)})
+
+class WebsiteUser(HttpUser):
+    tasks = [UserBehavior]
     min_wait = 1000
     max_wait = 3000
